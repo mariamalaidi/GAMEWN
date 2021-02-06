0f38167214d453ba1d45697a5049c9efc0aea25b
mariamalaidi <mariam.alaide@gmail.com>
2021-02-06 18:07:26 +0200

    Adding Font the Game

--- src/main/java/com/world/game/graphics/Animation.java
+++ src/main/java/com/world/game/graphics/Animation.java
@@ -1,2 +1,82 @@
-package com.world.game.graphics;public class Animation {
+package com.world.game.graphics;
+
+import java.awt.image.BufferedImage;
+
+public class Animation {
+    private BufferedImage[] frames;
+    private int currentFrame;
+    private int numFrames;
+    private int count;
+    private int delay;
+    private int timesPlayed;
+
+    public Animation(BufferedImage[] frames){
+        timesPlayed = 0;
+        setFrames(frames);
+    }
+    public Animation(){
+        timesPlayed = 0;
+    }
+
+    public void setFrames(BufferedImage[] frames){
+        this.frames = frames;
+        currentFrame = 0;
+        count = 0 ;
+        timesPlayed = 0 ;
+        delay = 2;
+        numFrames = frames.length;
+    }
+    public void setDelay(int delay){
+        this.delay = delay;
+    }
+    public void setFrame(int frame){
+        this.currentFrame = frame;
+    }
+    public void setNumFrames(int frame){
+        this.numFrames = frame;
+    }
+
+    public void update(){
+        if(delay == -1){
+            return ;
+        }
+        count++;
+        if(count == delay){
+            currentFrame++;
+            count = 0;
+        }
+        if(currentFrame == numFrames){
+            currentFrame = 0 ;
+            timesPlayed++;
+        }
+    }
+
+    public int getDelay(){
+        return delay;
+    }
+    public int getFrame(){
+        return currentFrame;
+    }
+    public int getCount(){
+        return count;
+    }
+    public BufferedImage getImage(){
+        return frames[currentFrame];
+    }
+    public boolean hasPlayedOnce(){
+        return timesPlayed > 0;
+    }
+    public boolean hasPlayed(int i ){
+        return timesPlayed == i;
+    }
+
+
+
+
+
+
+
+
+
+
 }
