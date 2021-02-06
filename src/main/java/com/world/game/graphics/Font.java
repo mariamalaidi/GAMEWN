c4f21ff53a14db781c1fea3490594b29f249d6be
mariamalaidi <mariam.alaide@gmail.com>
2021-02-06 16:58:18 +0200

    Adding Font the Game

---
+++ src/main/java/com/world/game/graphics/Font.java
@@ -0,0 +1,104 @@
+package com.world.game.graphics;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Font {
+
+    private BufferedImage FONTSHEET = null;
+    private BufferedImage[][] spriteArray;
+    private final int TILE_SIZE = 32;
+    public int w;
+    public int h;
+    private int wLetter;
+    private int hLetter;
+
+    public Font(String file) {
+        w = TILE_SIZE;
+        h = TILE_SIZE;
+
+        System.out.println("Loading: " + file + "...");
+        FONTSHEET = loadFont(file);
+
+        wLetter = FONTSHEET.getWidth() / w;
+        hLetter = FONTSHEET.getHeight() / h;
+        loadFontArray();
+    }
+
+    public Font(String file, int w, int h) {
+        this.w = w;
+        this.h = h;
+
+        System.out.println("Loading: " + file + "...");
+        FONTSHEET = loadFont(file);
+
+        wLetter = FONTSHEET.getWidth() / w;
+        hLetter = FONTSHEET.getHeight() / h;
+        loadFontArray();
+    }
+
+    public void setSize(int width, int height) {
+        setWidth(width);
+        setHeight(height);
+    }
+
+    public void setWidth(int i) {
+        w = i;
+        wLetter = FONTSHEET.getWidth() / w;
+    }
+
+    public void setHeight(int i) {
+        h = i;
+        hLetter = FONTSHEET.getHeight() / h;
+    }
+
+    public int getWidth() {
+        return w;
+    }
+
+    public int getHeight() {
+        return h;
+    }
+
+    private BufferedImage loadFont(String file) {
+        BufferedImage sprite = null;
+        try {
+            sprite = ImageIO.read(new File((file)));
+            System.out.println(sprite);
+        } catch (Exception e) {
+            System.out.println("ERROR: could not load file: " + file);
+        }
+
+        return sprite;
+    }
+
+    public void loadFontArray() {
+        spriteArray = new BufferedImage[wLetter][hLetter];
+
+        for (int x = 0; x < wLetter; x++) {
+            for (int y = 0; y < hLetter; y++) {
+                spriteArray[x][y] = getLetter(x, y);
+            }
+        }
+    }
+
+    public BufferedImage getFontSheet() {
+        return FONTSHEET;
+    }
+
+    public BufferedImage getLetter(int x, int y) {
+        BufferedImage img = FONTSHEET.getSubimage(x * w , y * h , w, h);
+        return img;
+    }
+
+    public BufferedImage getLetter(char letter) {
+        int value = letter - 65;
+
+        int x = value % wLetter;
+        int y = value / wLetter;
+        System.out.println(x+" , "+y);
+        return getLetter(x, y);
+    }
+}
